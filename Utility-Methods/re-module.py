03da515eb19768eb336fe9a1ac1ae17d62ff6cf6
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-09-29 01:19:18 +0300

    Create re-module.py

---
+++ Utility-Methods/re-module.py
@@ -0,0 +1,205 @@
+import re
+
+str = "Furkan Aygur 22 Istanbul"
+# *********************************************
+# re module
+
+
+#
+# result = re.findall("Furkan", str)
+# print(result)
+#
+# resultlen = len(result)
+# print(resultlen)
+#
+# result = re.split(" ", str)
+# print(result)
+#
+# result = re.sub("\s", "-", str)
+# print(result)
+#
+# result = re.search("Furkan", str)
+# # result = result.span()
+# # result = result.start()
+# # result = result.end()
+# # result = result.group()
+# result = result.string
+# print(result)
+
+# *********************************************
+# regular expression
+
+"""
+
+    [] - Köşeli parantezler arasına yazılan bütün karakterler
+         aranır.
+
+         [abc] => a      : 1 match
+                  ac     : 2 match 
+                  Python : No matches
+
+         [a-e]  => [abcde]
+         [1-5]  => [12345]
+         [0-39] => [01239]   
+
+         [^abc] => abc dışındaki karakterler.
+         [^0-9] => rakam olmayan karakterler.
+
+"""
+
+result = re.findall("[abc]", str)
+print(result)
+
+result = re.findall("[a-t]", str)
+print(result)
+
+result = re.findall("[1-5]", str)
+print(result)
+
+result = re.findall("[^a-t]", str)
+print(result)
+
+"""
+    . - Her hangi bir tek karakteri belirtir.
+
+        .. => a    : No match
+              ab   : 1 match
+              abc  : 1 match
+              abcd : 2 matches
+
+
+"""
+
+result = re.findall("F....n", str)
+print(result)
+
+"""
+    ^ - Belirtilen string karakterlerle başlıyor mu ?
+
+    ^a => a:    1 match
+          abc:  1 match
+          bac:  No match
+
+"""
+
+result = re.findall("^F", str)
+print(result)
+
+"""
+    $ - Belirtilen karakterle bitiyor mu ?
+
+    a$ => a      : 1 match
+          lamba  : 1 match
+          Python : No match 
+
+"""
+
+result = re.findall("Istanbul$", str)
+print(result)
+
+"""
+     * - Bir karakterin sıfır ya da daha fazla sayıda olmasını 
+         kontrol eder.
+
+         ma*n => mn     : 1 match
+                 man    : 1 match
+                 maaan  : 1 match
+                 main   : No match (a' nın arkasına n gelmiyor.) 
+"""
+
+result = re.findall("Fu*n", str)
+print(result)
+
+"""
+     + - Bir karakterin bir ya da daha fazla sayıda olmasını 
+         kontrol eder.
+
+         ma+n => mn     : No match
+                 man    : 1 match
+                 maaan  : 1 match
+                 main   : No match (a' nın arkasına n gelmiyor.) 
+"""
+result = re.findall("Fu+n", str)
+print(result)
+
+"""
+    ? - Bir karakterin sıfır ya da bir kez olmasını 
+        kontrol eder.
+
+        ma+n => mn     : No match
+                man    : 1 match
+                maaan  : 1 match
+                main   : No match (a' nın arkasına n gelmiyor.) 
+"""
+result = re.findall("Fu?n", str)
+print(result)
+
+
+"""
+    {} - Karakter sayısını kontrol eder.
+
+        al{2}   => a karakterinin arkasına l karakteri 2 kez tekrarlamalı.
+        al{2,3} => a karakterinin arkasına l karakteri en az 2 en fazla 3 kez tekrarlamalı.
+        [0-9]{2,4} => en az 2 en çok 4 basamaklı sayılar.
+"""
+result = re.findall("[0-9]{2}", str)
+print(result)
+
+
+"""
+    | - alternatif seçeneklerden birinin gerçekleşmesi gerekir.
+
+        a|b => a ya da b
+
+            cde =>    no match
+            ade =>    1 match
+            acdbea => 3 match 
+"""
+
+"""
+    () - gruplamak için kullanılır.
+
+         (a|b|c)xz => a,b,c karakterlerinin arkasına xz gelmelidir.
+"""
+result = re.findall("(a|b)n", str)
+print(result)
+
+"""
+    \ - Özel karakterleri aramamızı sağlar.
+        \$a => $ karakterinin arkasına a karakterinin arar. Yani
+               $ regular exp. engine tarafından yorumlanmaz.
+
+    \A - Belirtilen karakter string in başında mı ?
+         \Athe => the string in başındamı
+
+        result = re.findall("\APython", str)
+        result = re.findall("saat\Z", str)
+
+    \Z - Belirtilen karakter string in sonunda mı ?
+         the\Z => the string in sonunda mı
+
+    \b - Belirtilen karakter kelimenin in başında ya da sonunda mı ?
+         \bthe => the kelimenin in başında mı?
+         the\b => the kelimenin in sonunda mı?
+
+    \B - Belirtilen karakter kelimenin in başında ya da sonunda değil mı ?
+         \Bthe => the kelimenin in başında değil mi?
+         the\B => the kelimenin in sonunda değil mi?
+
+    \d - [0-9] ile aynı anlama gelir yani rakamları arar.
+         \d => 12abc34
+
+    \D - [^0-9] ile aynı anlama gelir yani rakam olmayanları arar.
+         \D => 1ab44_50
+
+    \s - Boşluk karakterlerini arar.  
+    \S - Boşluk karakterleri dışındakiler.
+    \w - Alfabetik karakterler, rakamlar ve alt çizgi karakteri.
+    \W - \w nin tam tersi
+
+"""
+result = re.findall("\AFurkan", str)
+print(result)
+
+result = re.findall("Istanbul\Z", str)
+print(result)
