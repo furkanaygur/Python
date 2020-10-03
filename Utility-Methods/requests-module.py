e92b29d99b9f261036a91c60963a3e6589d3e2a3
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-10-04 02:20:32 +0300

    Create requests-module.py

---
+++ Utility-Methods/requests-module.py
@@ -0,0 +1,12 @@
+# if you dont have a requests packages, you have to install
+# pip install requests
+
+import requests
+import json
+
+result = requests.get("https://jsonplaceholder.typicode.com/todos")
+result = json.loads(result.text)
+
+for i in result:
+    if i["userId"] == 1:
+        print(i["title"])
