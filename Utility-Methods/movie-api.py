28109900b745aee646c8ca61770677044994b741
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-10-04 20:42:26 +0300

    Update movie-api.py

--- Utility-Methods/movie-api.py
+++ Utility-Methods/movie-api.py
@@ -5,7 +5,7 @@ import requests
 class theMovieDb:
     def __init__(self):
         self.api_url = "https://api.themoviedb.org/3/"
-        self.api_key = " "  # its your api key
+        self.api_key = ""  # its your api key
 
     def getPopulars(self):
         response = requests.get(f"{self.api_url}movie/popular?api_key={self.api_key}&language=en-US&page=1")
