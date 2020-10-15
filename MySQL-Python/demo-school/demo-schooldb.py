b81d8f708cb2e1c954e4df49ebdfc92a98946df4
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-10-15 03:22:06 +0300

    updates

--- MySQL-Python/demo-school/demo-schooldb.py
+++ MySQL-Python/demo-school/demo-schooldb.py
@@ -8,16 +8,23 @@ class Student:
     connection = connection
     cursor = connection.cursor()
 
-    def __init__(self, studentNumber, name, surName, birthdate, gender ):
-        self.studentNumber = studentNumber
-        self.name = name
-        self.surName = surName
-        self.birthdate = birthdate
-        self.gender = gender
-    
-    def saveStudent(self):
+    def __init__(self):
+        print("deneme")
+
+    def display(self):
+        sql = "select * from students"
+        Student.cursor.execute(sql)
+
+        try:
+            result = Student.cursor.fetchall()
+            for i in result:
+                print(f"ID: {i[0]} \t Student Number: {i[1]} \t Name Surname: {i[2]} {i[3]} \t Gender: {i[4]}")
+        except mysql.connector.Error as err:
+            print("Error", err)
+
+    def saveStudent(self, studentNumber, name, surName, birthdate, gender):
         sql = "INSERT INTO students(StudentNumber, Name, SurName, Birthdate, Gender) VALUES (%s, %s, %s, %s, %s)"
-        value = (self.studentNumber, self.name, self.surName, self.birthdate, self.gender)
+        value = (studentNumber, name, surName, birthdate, gender)
         Student.cursor.execute(sql,value)
 
         try:
@@ -25,35 +32,89 @@ class Student:
             print(f"{Student.cursor.rowcount } Added")
         except mysql.connector.Error as err:
             print("Error",err)
-        finally:
-            Student.connection.close()
 
+
+    # @staticmethod
+    # def saveStudents(students_list):
+    #     sql = "INSERT INTO students(StudentNumber, Name, SurName, Birthdate, Gender) VALUES (%s, %s, %s, %s, %s)"
+    #     values = students_list
+    #     Student.cursor.executemany(sql,values)
+
+    #     try:
+    #         Student.connection.commit()
+    #         print(f"{Student.cursor.rowcount } Added")
+    #     except mysql.connector.Error as err:
+    #         print("Error",err)
 
     @staticmethod
-    def saveStudents(students_list):
-        sql = "INSERT INTO students(StudentNumber, Name, SurName, Birthdate, Gender) VALUES (%s, %s, %s, %s, %s)"
-        values = students_list
-        Student.cursor.executemany(sql,values)
+    def updateStudent(studentNumber):
+        name = input("Name: ")
+        surName = input("SurName: ")
+        birthdate = input("Birtdate (YYYY,MM,DD): ")
+        gender = input("Gender (M/F) : ")
+
+        sql = "update students set Name = %s, SurName= %s, Birthdate = %s, Gender= %s where StudentNumber = %s"
+        values = (name, surName, birthdate, gender, studentNumber)
+        Student.cursor.execute(sql, values)
 
         try:
             Student.connection.commit()
-            print(f"{Student.cursor.rowcount } Added")
+            print(f" { Student.cursor.rowcount } updated")
         except mysql.connector.Error as err:
             print("Error",err)
-        finally:
-            Student.connection.close()
+    
+
+    @staticmethod
+    def deleteStudent(id):
+        sql = "delete from students where ID = %s"
+        value = (id,)
+        Student.cursor.execute(sql, value)
+
+        try:
+            Student.connection.commit()
+            print(f" { Student.cursor.rowcount } deleted")
+        except mysql.connector.Error as err:
+            print("Error",err)
 
 # ******************* Main *************************
 
-# furkan = Student("111","Furkan", "Aygur", datetime(1998,8,2), "E")
+
+# furkan = Student("111","Furkan", "Aygur", datetime(1998,8,2), "M")
 # furkan.saveStudent()
 
-students_list = [
-    ("201","Furkan", "Aygur", datetime(1998,8,2), "E"),
-    ("202","Enes", "Aygur", datetime(1996,6,8), "E"),
-    ("203","Burak", "Aygur", datetime(1994,10,25), "E"),
-    ("204","Talha", "Aydin", datetime(1998,2,18), "E")
-]
+# students_list = [
+#     ("201","Furkan", "Aygur", datetime(1998,8,2), "M"),
+#     ("202","Enes", "Aygur", datetime(1996,6,8), "M"),
+#     ("203","Burak", "Aygur", datetime(1994,10,25), "M"),
+#     ("204","Talha", "Aydin", datetime(1998,2,18), "M")
+# ]
 
-Student.saveStudents(students_list)
+# Student.saveStudents(students_list)
+
+
+student = Student()
+while True :
+    print(" MENU ".center(50,'*'))
+    choice = input("1- Show Students \n2- Insert Student \n3- Update Student \n4- Delete Student \n5- Exit \nYour Choice: ")
+    if choice == '5':
+        break
+    elif choice == '1':
+        student.display()
+    elif choice == '2':
+        StudentNubmer = float(input("Studen Number: "))
+        name = input("Name: ")
+        surName = input("SurName: ")
+        birthdate = input("Birtdate (YYYY,MM,DD): ")
+        gender = input("Gender (M/F) : ")
+        student.saveStudent(StudentNubmer, name, surName, birthdate, gender)
  
+    elif choice == '3':
+        StudentNubmer = input("Student Number: ")
+        student.updateStudent(StudentNubmer)
+
+    elif choice == '4':
+        id = input("ID: ")
+        student.deleteStudent(id)
+
+    else:
+        print(" Only (1, 2, 3, 4, 5) ".center(50,'!'))
--- MySQL-Python/demo-school/schooldb_connection.py
+++ MySQL-Python/demo-school/schooldb_connection.py
@@ -3,6 +3,6 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="admin",
     database="schooldb"
 )
