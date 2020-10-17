b78df881e8197375e892bc73891d0958bf1a16c1
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-10-17 23:54:23 +0300

    updates

--- MySQL-Python/schooldb-app/Teachers.py
+++ MySQL-Python/schooldb-app/Teachers.py
@@ -1,8 +1,18 @@
 class Teachers:
-    def __init__(self, id, branch, name, surname, birtdate, gender, classid):
+    def __init__(self, id, branch, name, surname, birthdate, gender):
         self.id = id
         self.branch = branch
         self.name = name
         self.surname = surname
-        self.birtdate = birtdate
+        self.birthdate = birthdate
         self.gender = gender
+    
+    @staticmethod
+    def CreateTeacher(obj):
+        List = []
+        if isinstance(obj, tuple):
+            List.append(Teachers(obj[0], obj[1], obj[2], obj[3], obj[4], obj[5]))
+        else:
+            for i in obj:
+                List.append(Teachers(i[0], i[1], i[2], i[3], i[4], i[5]))
+        return List   
--- MySQL-Python/schooldb-app/db-manager.py
+++ MySQL-Python/schooldb-app/db-manager.py
@@ -2,7 +2,7 @@ import mysql.connector
 from datetime import datetime
 from connection import connection
 from Students import Students
-# from Teachers import Teachers
+from Teachers import Teachers
 # from Lessons import Lessons
 # from Classes import Classes
 
@@ -59,12 +59,37 @@ class DbManager:
             print("Error",err)
 
 
+    def getTeacherById(self, id):
+        sql = "select * from teachers where ID = %s"
+        value = (id,)
+        self.cursor.execute(sql,value)
+        try:
+            obj = self.cursor.fetchone()
+            return Teachers.CreateTeacher(obj)
+        except mysql.connector.Error as err:
+            print("Error", err) 
 
-    # def addTeacher(self, Teachers: teacher ):
-    #     pass
+    def addTeacher(self, Teachers: Teachers ):
+        sql = "INSERT INTO teachers(Branch, Name, SurName, Birthdate, Gender) VALUES (%s, %s, %s, %s, %s)"
+        value = (Teachers.branch, Teachers.name, Teachers.surname, Teachers.birthdate, Teachers.gender)
+        self.cursor.execute(sql,value)
 
-    # def updateTeacher(self, Teachers: teacher):
-    #     pass
+        try:
+            self.connection.commit()
+            print(f"{self.cursor.rowcount } Added")
+        except mysql.connector.Error as err:
+            print("Error",err)
+
+    def updateTeacher(self, Teachers: Teachers):
+        sql = "update teachers set Branch = %s, Name = %s, SurName= %s, Birthdate = %s, Gender= %s where ID = %s"
+        values = (Teachers.branch, Teachers.name, Teachers.surname, Teachers.birthdate, Teachers.gender, Teachers.id)
+        self.cursor.execute(sql, values)
+
+        try:
+            self.connection.commit()
+            print(f" { self.cursor.rowcount } updated")
+        except mysql.connector.Error as err:
+            print("Error",err)
     
     def closeConnection(self):
         self.connection.close()
@@ -81,20 +106,36 @@ db = DbManager()
 # print(student.name, student.surname)
 # db.closeConnection()
 
+# get teacher by id
+# teacher = db.getTeacherById(2)
+# print(teacher[0].name)
+
+
 # get student by class id
 # student = db.getStudentByClassId(1)
 # print(student[0].name)
 # print(student[1].name)
 # db.closeConnection()
 
-# Add
+# Add student
 # std = Students(None, 207, "Furkan", "Aygur", datetime(1998,2,8), "M", 1)
 # db.addStudent(std)
 # db.closeConnection()
 
-# Update
-student = db.getStudentById(50)
-student[0].name = "Furkan"
-student[0].surname = "AYGUR"
-db.updateStudent(student[0])
+# Add teacher
+# teacher = Teachers(None,"physics", "Furkan", "Aygur", datetime(1990,8,2),"M")
+# db.addTeacher(teacher)
+# db.closeConnection()
+
+# Update student
+# student = db.getStudentById(50)
+# student[0].name = "Furkan"
+# student[0].surname = "AYGUR"
+# db.updateStudent(student[0])
+# db.closeConnection()
+
+# Update teacher
+teacher = db.getTeacherById(4)
+teacher[0].name = "FURKAN"
+db.updateTeacher(teacher[0])
 db.closeConnection()
